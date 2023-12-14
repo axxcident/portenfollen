a261a79d3863e385668a3b591881c13b22b9e022
Axel Olivecrona <olivecrona.axel@gmail.com>
2023-12-14 12:15:04 +0100

    nu funkar det någorlunda

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,16 +1,46 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 
 //sidans namn blir portfolio efter namnet på javascript-filen
-const SecondPage = () => (
+const SecondPage = () => {
+
+
+
+  const data = useStaticQuery(graphql`
+  query {
+    allContentfulPosts {
+      edges {
+        node {
+          titel
+          description {
+            childrenMarkdownRemark {
+              excerpt
+            }
+          }
+          bild {
+            gatsbyImage(width: 200)
+          }
+        }
+      }
+    }
+  }
+  `
+  )
+
+  return (
+
   <Layout>
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
+    <pre>{JSON.stringify({data}, null, 2)}</pre>
   </Layout>
-)
+  )
+}
 
 export const Head = () => <title>Portfolio Page</title>
 
