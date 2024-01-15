0631e39ca02fc4b62281a7b82c0b101e3334d353
Axel Olivecrona <112471629+axxcident@users.noreply.github.com>
2024-01-15 16:46:47 +0100

    sökbar synlig

--- src/hooks/useNavigation.js
+++ src/hooks/useNavigation.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { graphql, useStaticQuery, Link } from "gatsby";
 import { useLocation } from "@reach/router";
+import { FaSearch } from "react-icons/fa";
 
 const useNavigation = () => {
   const data = useStaticQuery(graphql`
@@ -19,14 +20,22 @@ const useNavigation = () => {
 
   const location = useLocation();
   const urls = ["/", "/about/", "/contact/", "/portfolio/"]
+  const [isSearchActive, setIsSearchActive] = useState(false);
+  const handleSearch = () => {
+    setIsSearchActive(!isSearchActive);
+  }
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      handleSearch();
+    }
+  };
 
   return (
     <>
     <nav>
-      <ul className="headerlink-navigation">
+      <ul className={`headerlink-navigation ${isSearchActive ? "widebar" : ""}`}>
       {data.allContentfulPage.edges.map((edge, index) => (
         <li key={edge.node.id}>
-          {/* <Link className="headerlink" to={`${edge.node.url}`}>{edge.node.titel}</Link> edge.node.url+"/" */}
           <Link
               className={`headerlink${location.pathname === urls[index] || (location.pathname.includes("/portfolio-post/") && edge.node.url === "/portfolio") ? " active" : ""}`}
               to={`${edge.node.url}`}
@@ -36,17 +45,23 @@ const useNavigation = () => {
           </Link>
         </li>
       ))}
+        <li className={`search-icon ${isSearchActive ? "active" : ""}`}
+            onClick={handleSearch}
+            onKeyPress={handleKeyPress}
+            role="button"
+            tabIndex="0"
+            >
+          <FaSearch />
+        </li>
+        {isSearchActive && (
+        <div className="search-input-container">
+          <input type="text" className="search-bar" />
+        </div>
+          )}
       </ul>
     </nav>
     </>
   );
-
-  // return data.allContentfulPage.edges.map((edge) => ({
-  //   id: edge.node.id,
-  //   titel: edge.node.titel,
-  //   url: edge.node.url,
-  // }));
-
 };
 
 export default useNavigation;
