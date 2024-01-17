ca455f99f97e037d09a853c33e5b9e054293badc
Axel Olivecrona <olivecrona.axel@gmail.com>
2024-01-17 11:25:42 +0100

    kontakt ser bättre ut nu

--- src/templates/contact-template.js
+++ src/templates/contact-template.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { GatsbyImage, getImage  } from "gatsby-plugin-image"
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import { BsLinkedin } from "react-icons/bs";
 import { FaGithubSquare } from "react-icons/fa";
@@ -16,76 +17,80 @@ const ContactPage = (contentfulPage) => {
       <div className="gradient-background-1"></div>
       <div className="kontakt-container">
         {documentToReactComponents(JSON.parse(contentfulPage.content.raw))}
+        <p className="contact-intro">Om du har frågor om de kurser jag förärvat och vill komma i kontakt med mig så finns det många sätt att kontakta mig på</p>
+        <GatsbyImage className="kontakt-bild" image={getImage(contentfulPage.bilden.gatsbyImage)} alt={contentfulPage.titel} />
         <div className="kontakt">
+          <p className="contact-paragrah">Hitta mig på sociala medier, ladda ner mitt CV eller kontakta mig genom detta formulär</p>
+          <div className="cta-knappar">
           <a
             href="/Axel_O_CV.pdf"
             download={true}
             className="group bg-white px-7 py-4 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10 dark:bg-white/10"
           >
-            Ladda ner CV
+            <span className="link-span">Ladda ner CV</span>
             <HiDownload className="opacity-70 group-hover:translate-y-0.5 transition" />
           </a>
-
           <a
             href="https://www.linkedin.com/in/axel-olivecrona-077b17b8/"
             target="_blank"
             rel="noopener noreferrer"
             className="bg-white text-gray-700 hover:text-gray-950 p-4 flex items-center gap-2 rounded-full cursor-pointer border border-black/10 focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition dark:bg-white/10 dark:text-white/60"
           >
+            <span className="link-span">LinkedIn</span>
             <BsLinkedin />
           </a>
-
           <a
             href="https://github.com/axxcident"
             target="_blank"
             rel="noopener noreferrer"
             className="bg-white text-gray-700 hover:text-gray-950 text-[1.35rem] p-4 flex items-center gap-2 rounded-full cursor-pointer border border-black/10 focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition dark:bg-white/10 dark:text-white/60"
           >
+            <span className="link-span">Github</span>
             <FaGithubSquare />
           </a>
-        </div>
-        <div className="email-container">
-          <h4>eller kontakta mig genom detta formulär</h4>
-          <p className="">
-            Please contact me directly at{" "}
-            <a className="" href="mailto:olivecrona.axel@gmail.com">olivecrona.axel@gmail.com</a>
-            {" "}or through this form.
-          </p>
-          <form
-            name="contact"
-            // method="POST"
-            // data-netlify="true"
-            className="contact-form">
-            <input
-              className="form-input"
-              name="senderEmail"
-              type="email"
-              required
-              maxLength={500}
-              placeholder="Email"
-            />
-            <textarea
-              className="textarea-input"
-              name="message"
-              placeholder="Subject"
-              required
-              maxLength={500}
-            />
-            <button
-              type="submit"
-              className="kontakt-btn"
-              disabled={pending}
-              >
-              {pending ? (
-                <div className=""></div>
-              ) : (
-                <>
-                  Send{" "}
-                  <FaPaperPlane className="" />{" "}
-                </>
-              )}
-            </button>
-          </form>
+          </div>
+          <div className="email-container">
+            <form
+              name="contact"
+              className="contact-form">
+              <input
+                className="form-input"
+                name="senderEmail"
+                type="email"
+                required
+                maxLength={500}
+                placeholder="Email"
+              />
+              <textarea
+                className="textarea-input"
+                name="message"
+                placeholder="Subject"
+                required
+                maxLength={500}
+              />
+              <div className="form-bottom">
+                <button
+                  type="submit"
+                  className="kontakt-btn"
+                  disabled={pending}
+                  >
+                  {pending ? (
+                    <div className=""></div>
+                  ) : (
+                    <>
+                      Send{" "}
+                      <FaPaperPlane className="" />{" "}
+                    </>
+                  )}
+                </button>
+                <p className="email-text">
+                  Please contact me directly at{" "}
+                  <a className="email-anchor" href="mailto:olivecrona.axel@gmail.com">olivecrona.axel@gmail.com</a>
+                  {" "}or through this form.
+                </p>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
       <div className="gradient-background-3"></div>
